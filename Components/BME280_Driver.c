2178aeda3ee344b51965e1c7952c5a0e18895ac4
Xavier McX <x@y.com>
2020-07-11 03:57:22 +0100

    -mbme registers

--- Components/BME280_Driver.c
+++ Components/BME280_Driver.c
@@ -7,7 +7,9 @@
 ****************************************/
 
 /********* Includes *******************/
-#include <>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 #include ""
 
 /****** Function Prototypes ***********/
@@ -18,6 +20,56 @@
 
 /****** Private Functions *************/
 
+/* Returns temperature in DegC, resolution is 0.01 DegC. Output value of “5123” equals 51.23 DegC. */
+/* t_fine carries fine temperature as global valueint32_tt_fine; */
+int32_t BME280_compensate_T_int32(int32_t adc_T)
+{
+    int32_t var1, var2, T;
+
+    var1 = ((((adc_T >> 3) - ((int32_t)dig_T1 << 1))) * ((int32_t)dig_T2)) >> 11;
+    var2 = (((((adc_T >> 4) - ((int32_t)dig_T1)) * ((adc_T >> 4) - ((int32_t)dig_T1))) >> 12) * ((int32_t)dig_T3)) >> 14;
+    t_fine = var1 + var2;
+    T = (t_fine * 5 + 128) >> 8;
+    return T;
+}
+
+/* Returns pressure in Pa as unsigned 32 bit integer in Q24.8 format (24 integer bits and 8 fractional bits).*/
+/* Output value of “24674867” represents 24674867 / 256 = 96386.2 Pa = 963.862 hPa */
+uint32_t BME280_compensate_P_int64(int32_t adc_P)
+{
+    int64_t a;
+    int64_t var1, var2, p;
+    var1 = ((int64_t)t_fine) - 128000;
+    var2 = var1 * var1 * (int64_t)dig_P6;
+    var2 = var2 + ((var1 * (int64_t)dig_P5) << 17);
+    var2 = var2 + (((int64_t)dig_P4) << 35);
+    var1 = ((var1 * var1 * (int64_t)dig_P3) >> 8) + ((var1 * (int64_t)dig_P2) << 12);
+    var1 = (((((int64_t)1) << 47) + var1)) * ((int64_t)dig_P1) >> 33;
+    if (var1 == 0)
+    {
+        return0; // avoid exception caused by division by zero
+    }
+    p = 1048576 - adc_P;
+    p = (((p << 31) - var2) * 3125) / var1;
+    var1 = (((int64_t)dig_P9) * (p >> 13) * (p >> 13)) >> 25;
+    var2 = (((int64_t)dig_P8) * p) >> 19;
+    p = ((p + var1 + var2) >> 8) + (((int64_t)dig_P7) << 4);
+    return (uint32_t)p;
+}
+
+// Returns humidity in %RH as unsigned 32 bit integer in Q22.10 format (22 integer and 10 fractional bits).
+// Output value of “47445” represents 47445/1024 = 46.333 %RH
+
+uint32_t bme280_compensate_H_int32(int32_t adc_H)
+{
+    int32_t v_x1_u32r;
+    v_x1_u32r = (t_fine - ((int32_t)76800));
+    v_x1_u32r = (((((adc_H << 14) - (((int32_t)dig_H4) << 20) - (((int32_t)dig_H5) * v_x1_u32r)) + ((int32_t)16384)) >> 15) * (((((((v_x1_u32r * ((int32_t)dig_H6)) >> 10) * (((v_x1_u32r * ((int32_t)dig_H3)) >> 11) + ((int32_t)32768))) >> 10) + ((int32_t)2097152)) * ((int32_t)dig_H2) + 8192) >> 14));
+    v_x1_u32r = (v_x1_u32r - (((((v_x1_u32r >> 15) * (v_x1_u32r >> 15)) >> 7) * ((int32_t)dig_H1)) >> 4));
+    v_x1_u32r = (v_x1_u32r < 0 ? 0 : v_x1_u32r);
+    v_x1_u32r = (v_x1_u32r > 419430400 ? 419430400 : v_x1_u32r);
+    return (uint32_t)(v_x1_u32r >> 12);
+}
 /****** Global Data *******************/
 
 /****** Global Functions *************/
--- Components/inc/BME280_Driver.h
+++ Components/inc/BME280_Driver.h
@@ -1,8 +1,9 @@
 /****************************************
-* \file
-* \brief
-* \date
-* \author
+* \file     BME280_Driver.h
+* \brief    Header file for the BME280 bosch temperature sensor
+*           Driver.
+* \date     July 2020 
+* \author   RJAM
 ****************************************/
 
 #ifndef BME280_DRIVER_H
@@ -11,6 +12,49 @@
 /********* Includes ********************/
 
 /********* Definitions *****************/
+
+#define BMP_I2C_ADDRESS_SDLOW 0x76
+#define BMP_I2C_ADDRESS_SDHIGH 0x77
+
+#define BMP_TRANSACTION_READ_BIT 0x01
+
+#define BMP_REG_ADDR_DIGT1_LSB 0x88
+#define BMP_REG_ADDR_DIGT1_MSB 0x89
+#define BMP_REG_ADDR_DIGT2_LSB 0x8A
+#define BMP_REG_ADDR_DIGT2_MSB 0x8B
+#define BMP_REG_ADDR_DIGT3_LSB 0x8C
+#define BMP_REG_ADDR_DIGT3_MSB 0x8D
+#define BMP_REG_ADDR_DIGP1_LSB 0x8E
+#define BMP_REG_ADDR_DIGP1_MSB 0x8F
+#define BMP_REG_ADDR_DIGP2_LSB 0x90
+#define BMP_REG_ADDR_DIGP2_MSB 0x91
+#define BMP_REG_ADDR_DIGP3_LSB 0x92
+#define BMP_REG_ADDR_DIGP3_MSB 0x93
+#define BMP_REG_ADDR_DIGP4_LSB 0x94
+#define BMP_REG_ADDR_DIGP4_MSB 0x95
+#define BMP_REG_ADDR_DIGP5_LSB 0x96
+#define BMP_REG_ADDR_DIGP5_MSB 0x97
+#define BMP_REG_ADDR_DIGP6_LSB 0x98
+#define BMP_REG_ADDR_DIGP6_MSB 0x99
+#define BMP_REG_ADDR_DIGP7_LSB 0x9A
+#define BMP_REG_ADDR_DIGP7_MSB 0x9B
+#define BMP_REG_ADDR_DIGP8_LSB 0x9C
+#define BMP_REG_ADDR_DIGP8_MSB 0x9D
+#define BMP_REG_ADDR_DIGP9_LSB 0x9E
+#define BMP_REG_ADDR_DIGP9_MSB 0x9F
+#define BMP_REG_ADDR_DIGH1 0xA1
+#define BMP_REG_ADDR_DIGH2_LSB 0xE1
+#define BMP_REG_ADDR_DIGH2_MSB 0xE2
+#define BMP_REG_ADDR_DIGH3 0xE3
+#define BMP_REG_ADDR_DIGH4_LSB 0xE4
+#define BMP_REG_ADDR_DIGH4_MSB 0xE5
+#define BMP_REG_ADDR_DIGH5_LSB 0xE5
+#define BMP_REG_ADDR_DIGH5_MSB 0xE6
+#define BMP_REG_ADDR_DIGH6 0xE7
+/**
+ *  E4:   -  digH4[11:4]
+ *  E5:   -  digH4[3:0]
+ * 
 
 /********** Types **********************/
 
