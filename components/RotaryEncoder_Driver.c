6baa3d6650f08c8921ad0722341562dabc090903
Xavier McX <x@y.com>
2020-07-26 02:46:25 +0100

    more functions, etc.

--- components/RotaryEncoder_Driver.c
+++ components/RotaryEncoder_Driver.c
@@ -1,5 +1,5 @@
 /***************************************
-* \file RotaryEncoder_Driver.c
+* \file     RotaryEncoder_Driver.c
 * \brief    An ESP32 IDF / FreeRTOS driver for a standard incremental Rotary encoder
 *           Driver is based on GPIO driver, ESP interrupts and (maybe) freertos timer
 *           Also includes support for central button
@@ -50,9 +50,17 @@ esp_err_t rotaryEncoderInit(gpio_num_t dataPin, gpio_num_t clockPin, gpio_num_t 
 
     esp_err_t initStatus = ESP_OK;
 
+    /** configure the pins **/
     gpio_config_t pinConfig;
     pinConfig.mode = GPIO_MODE_INPUT;
-    pinConfig.pin_bit_mask = dataPin;
+    if (enableButton)
+    {
+        pinConfig.pin_bit_mask = (dataPin | enableButton);
+    }
+    else
+    {
+        pinConfig.pin_bit_mask = dataPin;
+    }
     pinConfig.intr_type = GPIO_INTR_ANYEDGE;
     pinConfig.pull_down_en = 0;
     pinConfig.pull_up_en = 0;
@@ -64,7 +72,12 @@ esp_err_t rotaryEncoderInit(gpio_num_t dataPin, gpio_num_t clockPin, gpio_num_t 
         pinConfig.pin_bit_mask = clockPin;
         initStatus = gpio_config(&pinConfig);
     }
+    else
+    {
+        ESP_LOGE(RE_TAG, "Error configuring data/button pin(s): 0x%x", initStatus);
+    }
 
+    /** configure the interrupts **/
     if (initStatus == ESP_OK)
     {
         if (installISR)
@@ -78,7 +91,6 @@ esp_err_t rotaryEncoderInit(gpio_num_t dataPin, gpio_num_t clockPin, gpio_num_t 
         }
         if (initStatus == ESP_OK)
         {
-
             initStatus = gpio_isr_handler_add(dataPin, &RE_DataInterrupt, NULL);
         }
         if (initStatus != ESP_OK)
@@ -89,14 +101,106 @@ esp_err_t rotaryEncoderInit(gpio_num_t dataPin, gpio_num_t clockPin, gpio_num_t 
         {
             initStatus = gpio_isr_handler_add(btnPin, &RE_BtnInterrupt, NULL);
         }
+        if (initStatus != ESP_OK)
+        {
+            ESP_LOGE(RE_TAG, "Error adding button gpio isr handler: 0x%x", initStatus);
+        }
     }
 
-    memset(&reControl, 0, sizeof(rotaryEncoder_t));
-    reControl.clockPinNum = clockPin;
-    reControl.dataPinNum = dataPin;
-    reControl.buttonEnabled = enableButton;
-    reControl.stepSize = 1;
-    reControl.count.uValue = (UINT16_MAX / 2); /** start the counter in middle of value range **/
-
+    if (initStatus == ESP_OK)
+    {
+        memset(&reControl, 0, sizeof(rotaryEncoder_t));
+        reControl.clockPinNum = clockPin;
+        reControl.dataPinNum = dataPin;
+        reControl.buttonEnabled = enableButton;
+        reControl.stepSize = 1;
+        reControl.count.uValue = (UINT16_MAX / 2); /** start the counter in middle of value range **/
+    }
     return initStatus;
+}
+
+esp_err_t rotaryEncoder_attachToTask(TaskHandle_t parentTask, bool alertStep, bool alertButton)
+{
+
+    esp_err_t status = ESP_OK;
+
+    if (reControl.parentTask != NULL)
+    {
+        ESP_LOGI(RE_TAG, "Replacing task handle...");
+    }
+
+    reControl.parentTask = parentTask;
+    if (alertStep)
+    {
+        reControl.alertStep = true;
+    }
+    if (reControl.buttonEnabled && alertButton)
+    {
+        reControl.alertBtn = true;
+    }
+
+    return status;
+}
+
+esp_err_t rotaryEncoder_setSignedCounter()
+{
+
+    esp_err_t status = ESP_OK;
+
+    if (!(reControl.signedCounter))
+    {
+        reControl.signedCounter = true;
+    }
+
+    return status;
+}
+
+esp_err_t rotaryEncoder_getValue(uint16_t *value)
+{
+    if (reControl.signedCounter)
+    {
+        *value = reControl.count.Value;
+    }
+    else
+    {
+        *value = reControl.count.uValue;
+    }
+
+    return ESP_OK;
+}
+
+esp_err_t rotaryEncoder_getLastDir(uint8_t *lastDir)
+{
+    *lastDir = reControl.dirLast;
+    return ESP_OK;
+}
+
+esp_err_t rotaryEncoder_getButtonState(uint8_t *state)
+{
+    *state = (uint8_t)reControl.btnState;
+    return ESP_OK;
+}
+
+esp_err_t rotaryEncoder_getButtonPressT(uint32_t *btnPressT)
+{
+    *btnPressT = reControl.tBtnPress;
+    return ESP_OK;
+}
+
+esp_err_t rotaryEncoder_setCounterMax(uint16_t countMax)
+{
+
+    esp_err_t setStatus = ESP_OK;
+
+    if (reControl.signedCounter && countMax > INT16_MAX)
+    {
+        ESP_LOGE(RE_TAG, "Error: In signed mode, max value is %d", INT16_MAX);
+        setStatus = ESP_ERR_INVALID_SIZE;
+    }
+    else
+    {
+        reControl.counterMax = countMax;
+    }
+
+    return ESP_OK;
 }
--- components/include/RotaryEncoder_Driver.h
+++ components/include/RotaryEncoder_Driver.h
@@ -19,6 +19,13 @@ typedef union {
     int16_t Value;   /** < rotary encoder value (signed version ) **/
 } rotaryCount;
 
+typedef buttonDebouce
+{
+    uint32_t tDebounce;
+    uint32_t tLastPress;
+    bool debounceActive;
+}
+
 typedef struct rotaryEncoder
 {
 
@@ -29,6 +36,9 @@ typedef struct rotaryEncoder
     bool btnDebounceState; /** < button debounce state **/
     bool dirLast;          /** < last turn direction 0 - c-clkwise, 1 - clkwise **/
     bool btnState;         /** < current state of button **/
+    bool alertBtn;         /** < send task notification if button pressed **/
+    bool alertStep;        /** < send task notification if step changes **/
+    bool halfBtnInterrupt; /** < send the notify on btn press, rather than release */
 
     gpio_num_t dataPinNum;  /** < data pin gpio  **/
     gpio_num_t clockPinNum; /** < clock pin gpio **/
@@ -36,6 +46,13 @@ typedef struct rotaryEncoder
     uint16_t stepSize;  /** < increment/decrement size **/
     uint32_t tLastStep; /** < time since the last rotary step **/
     uint32_t tLastBtn;  /** < time since last button push **/
+    uint32_t tBtnPress; /** < time the button is held down for **/
+
+    uint16_t counterMax;
+    uint16_t counterMin;
+
+    TaskHandle_t parentTask; /** < Task to notify **/
+
 } rotaryEncoder_t;
 
 /********** Types **********************/
