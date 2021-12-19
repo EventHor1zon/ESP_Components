0e4be07dcd99850a8346b2483a5000187b20aaa8
EventHor1zon <3v3ntH0r1z0n@protonmail.com>
2021-12-19 22:26:35 +0000

    init & spi interface, works for reading!

--- MFRC522_Driver.c
+++ MFRC522_Driver.c
@@ -7,13 +7,18 @@
 ****************************************/
 
 /********* Includes *******************/
+#include "string.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_types.h"
 #include "esp_err.h"
 #include "esp_log.h"
 #include "Utilities.h"
+#include "driver/gpio.h"
+#include "driver/spi_common.h"
+#include "driver/spi_master.h"
 #include "genericCommsDriver.h"
+#include "Utilities.h"
 
 #include "MFRC522_Driver.h"
 
@@ -21,6 +26,70 @@
 
 const char *MFRC_TAG = "MFRC Driver";
 
+const mfrc_register_t init_reg_state = {
+    .command_reg.regval = 0x20,
+    .command_en_reg.regval = 0x80,
+    .irq_passing_reg.regval = 0x00,
+    .com_irq_reg.regval = 0x14,
+    .div_irq_reg.regval = 0x00,
+    .error_reg.regval = 0x00,
+    .status_1_reg.regval = 0x21,
+    .status_2_reg.regval = 0x00,
+    .fifo_data_reg.regval = 0x00,
+    .fifo_lvl_reg.regval = 0x00,
+    .fifo_watermark_reg.regval = 0x08,
+    .control_reg.regval = 0x10,
+    .bitframe_reg.regval = 0x00,
+    .collision_reg.regval = 0x00,
+    .reserved_1 = 0x00,
+    .reserved_2 = 0x00,
+    .mode_reg.regval = 0x3F,
+    .tx_mode_reg.regval = 0x00,
+    .rx_mode_reg.regval = 0x00,
+    .txcontrol_reg.regval = 0x80,
+    .tx_ask_reg.regval = 0x00,
+    .tx_select_reg.regval = 0x10,
+    .rx_select_reg.regval = 0x84,
+    .rx_threshold_reg.regval = 0x84,
+    .demod_reg.regval = 0x4D,
+    .reserved_3 = 0x00,
+    .reserved_4 = 0x00,
+    .mifare_tx_reg.regval = 0x62,
+    .mifare_rx_reg.regval = 0x00,
+    .reserved_5 = 0x00,
+    .serial_speed_reg.regval = 0xEB,
+    .reserved_7 = 0,
+    .crc_result_msb = 0,
+    .crc_result_lsb = 0,
+    .reserved_8 = 0,
+    .mod_width = 0,
+    .reserved_9 = 0,
+    .rf_cfg_reg.regval = 0x48,
+    .gsn_reg.regval = 0x88,
+    .cw_gsp_reg.regval = 0x20,
+    .mod_gs_reg.regval = 0x20,
+    .tmr_mode_reg.regval = 0x00,
+    .tmr_prescaler_lsb = 0,
+    .tmr_reload_msb = 0,
+    .tmr_reload_lsb = 0,
+    .tmr_cnt_msb = 0,
+    .tmr_cnt_lsb = 0,
+    .reserved_10 = 0,
+    .test_sel_1_reg.regval = 0,
+    .test_sel_2_reg.regval = 0,
+    .testpin_en_reg.regval = 0x80,
+    .testpin_val_reg.regval = 0,
+    .testbus = 0,
+    .auto_test_reg.regval = 0,
+    .version_reg.regval = 0,
+    .analog_test_reg.regval = 0,
+    .test_dac1_reg.regval = 0,
+    .test_dac2_reg.regval = 0,
+    .adc_test_reg.regval = 0,
+    .reserved_11 = 0,
+    .reserved_12 = 0,
+    .reserved_13 = 0
+};
 /****** Function Prototypes ***********/
 
 /************ ISR *********************/
@@ -33,6 +102,129 @@ void mfrc_irq_function(void *args) {
 /****** Private Data ******************/
 
 /****** Private Functions *************/
+
+/*
+ * Resets the driver's record of the registers to the post-reset state 
+ */
+static void mfrc_reset_registers(MFRC_DEV dev) {
+    memcpy(&dev->registers, &init_reg_state, sizeof(mfrc_register_t));
+    return;
+}
+
+// static esp_err_t mfrc_test_registers(MFRC_DEV dev) {
+
+//     esp_err_t err = ESP_OK;
+
+//     uint8_t tx_buffer[64] = {0};
+//     uint8_t rx_buffer[64] = {0};
+
+//     for(uint8_t i=0; i < 64; i++) {
+//         tx_buffer[i] = (((i+1) << 1) | MFRC_SPI_READ_BIT);
+//     }
+
+//     spi_transaction_t trx = {0};
+//     trx.length = 8 * 64;
+//     trx.rxlength = 8 * 64;
+//     trx.tx_buffer = tx_buffer;
+//     trx.rx_buffer = rx_buffer;
+
+//     err = spi_device_polling_transmit(dev->spi_handle, &trx);
+
+//     if(!err) {
+//         printf("Tx Block\n");
+//         showmem(tx_buffer, 64);
+//         printf("Rx Block\n");
+//         showmem(rx_buffer, 64);
+//     }
+
+//     return err;
+// }
+
+
+
+
+
+static esp_err_t mfrc_read_register_byte(MFRC_DEV dev, uint8_t reg_addr, uint8_t *data) {
+
+    esp_err_t err = ESP_OK;
+
+    spi_transaction_t trx = {0};
+    trx.length = 16;
+    trx.tx_data[0] = (reg_addr << MFRC_SPI_ADDR_SHIFT) | MFRC_SPI_READ_BIT;
+    trx.flags = SPI_TRANS_USE_TXDATA | SPI_TRANS_USE_RXDATA ;
+
+    err = spi_device_polling_transmit(dev->spi_handle, &trx);
+
+    if(!err) {
+        *data = trx.rx_data[1];
+    }
+    else {
+        ESP_LOGE(MFRC_TAG, "Error during transmission {%u}", err);
+    }
+
+    return err;
+}
+
+static esp_err_t mfrc_write_register_byte(MFRC_DEV dev, uint8_t reg_addr, uint8_t data) {
+
+    esp_err_t err = ESP_OK;
+
+    spi_transaction_t trx = {0};
+    trx.length = 16;
+    trx.tx_data[0] = (reg_addr << MFRC_SPI_ADDR_SHIFT);
+    trx.tx_data[1] = data;
+    trx.flags = SPI_TRANS_USE_TXDATA;
+
+    err = spi_device_polling_transmit(dev->spi_handle, &trx);
+
+    if(err) {
+        ESP_LOGE(MFRC_TAG, "Error during transmission {%u}", err);
+    }
+
+    return err;
+}
+
+
+static esp_err_t mfrc_configure_spi(MFRC_DEV dev) {
+
+    esp_err_t err = ESP_OK;
+
+    if( 
+        dev->select_pin< 1  ||
+        dev->comms_mode != MFRC_SPI_COMMS_MODE ||
+        dev->comms_bus > VSPI_HOST
+    ) {
+        ESP_LOGE(MFRC_TAG, "Error - configuring SPI 4-wire mode");
+        err = ESP_ERR_INVALID_ARG;
+    }
+    else {
+        spi_device_interface_config_t cfg = {0};
+        cfg.clock_speed_hz = 100000;
+        cfg.spics_io_num = dev->select_pin;
+        cfg.queue_size = 10;
+        cfg.mode = 3;
+
+        err = spi_bus_add_device(dev->comms_bus, &cfg, (spi_device_handle_t *)&dev->spi_handle);
+    }
+
+    return err;
+}
+
+
+static esp_err_t mfrc_reset_device(MFRC_DEV dev) {
+
+    if(dev->rst_en) {
+        gpio_set_level(dev->rst_pin, 0);
+        vTaskDelay(pdMS_TO_TICKS(1));
+        gpio_set_level(dev->rst_pin, 1);
+    }
+    else {
+        ESP_LOGE(MFRC_TAG, "Device Reset pin not configured, cannot reset device");
+    }
+    return ESP_OK;
+}
+
+
 
 static esp_err_t mfrc_read_from_address(MFRC_DEV dev, uint8_t address, uint8_t len, uint8_t *buffer) {
 
@@ -70,25 +262,45 @@ MFRC_DEV mfrc_init(mfrc_init_t *init) {
     MFRC_DEV handle = NULL;
     gpio_config_t pin_config = {0};
     esp_err_t err = ESP_OK;
+    uint8_t data = 0;
 
-    if(init->comms_mode => MFRC_COMMS_MODE_INVALID) {
+
+    if(init->comms_mode >= MFRC_COMMS_MODE_INVALID) {
         err = ESP_ERR_INVALID_ARG;
     }
 
+
+    /** create handle **/
     if(!err) {
         handle = heap_caps_calloc(1, sizeof(MFRC522_Driver_t), MALLOC_CAP_DEFAULT);
         if(handle == NULL) {
             err = ESP_ERR_NO_MEM;
-            ESP_LOGE(MFRC_TAG, "Error - unable to assign heap memory for handle [%u]", );
+            ESP_LOGE(MFRC_TAG, "Error - unable to assign heap memory for handle [%u]", err);
         }
         else {
-            handle->comms_bus = init->comms_bus;
-            handle->comms_mode = init->comms_mode;
-            handle->irq_pin = init->irq_pin;
-            handle->rst_pin = init->rst_pin;
+            if(handle->comms_mode == MFRC_SPI_COMMS_MODE) {
+                handle->select_pin = init->sel;
+            }
+            else if(handle->comms_mode == MFRC_I2C_COMMS_MODE) {
+                handle->data_pin = init->sda;
+                handle->clock_pin = init->scl;
+            }
+            else {
+                ESP_LOGE(MFRC_TAG, "Error - only I2C and SPI comms modes supported");
+                err = ESP_ERR_INVALID_STATE;
+            }
+
+            if(!err) {
+                handle->comms_bus = init->comms_bus;
+                handle->comms_mode = init->comms_mode;
+                handle->irq_pin = init->irq_pin;
+                handle->rst_pin = init->rst_pin;
+                mfrc_reset_registers(handle);
+            }
         }
     }
 
+    /** initialise the pins **/
     if(!err) {
     
         pin_config.intr_type = GPIO_INTR_DISABLE;
@@ -104,6 +316,7 @@ MFRC_DEV mfrc_init(mfrc_init_t *init) {
                 ESP_LOGE(MFRC_TAG, "Error configuring reset pin {%u}", err);
             }
             else {
+                gpio_set_level(init->rst_pin, 1);
                 handle->rst_en = true;
             }
         }
@@ -123,11 +336,12 @@ MFRC_DEV mfrc_init(mfrc_init_t *init) {
                 else {
                     handle->irq_en = true;
                 }
-
             }
         }
     }
     
+
+    /** create task **/
     if(!err) {
         if(xTaskCreate(mrfc_driver_task, "mrfc_driver_task", 5012, handle, 3, &handle->task) != pdTRUE) {
             ESP_LOGE(MFRC_TAG, "Error creating driver task!");
@@ -135,7 +349,59 @@ MFRC_DEV mfrc_init(mfrc_init_t *init) {
         }
     }
 
+    /** add the device to the spi bus **/
+    if(handle->comms_mode == MFRC_SPI_COMMS_MODE) {
+        err = mfrc_configure_spi(handle);
+    }
+
+
+    if(err) {
+        ESP_LOGE(MFRC_TAG, "Error initialising MFRC522 driver [%u]", err);
+        if(handle) {
+            heap_caps_free(handle);
+        }
+    } 
+    else {
+        ESP_LOGI(MFRC_TAG, "Succesfully started MFRC522 Driver");
+        if(handle->rst_en) {
+            mfrc_reset_device(handle);
+            vTaskDelay(pdMS_TO_TICKS(2));
+        }
+        
+        
+        
+        mfrc_read_register_byte(handle, 1, &data);
+        mfrc_read_register_byte(handle, 1, &data);
+        mfrc_read_register_byte(handle, 2, &data);
+        mfrc_read_register_byte(handle, 3, &data);
+    }
 
     return handle;
-
 }
+
+
+
+
+void mfrc_deinit(MFRC_DEV dev) {
+
+    if(dev != NULL) {
+        heap_caps_free(dev);
+    }
+
+    return;
+}
+
+
+
+// esp_err_t mfrc_set_interrupt_mask(MFRC_DEV dev, uint8_t *intr);
+
+// esp_err_t mfrc_get_interrupt_mask(MFRC_DEV dev, uint8_t *intr);
+
+// esp_err_t mfrc_enable_interrupt_pin(MFRC_DEV dev);
+
+// esp_err_t mfrc_disable_interrupt_pin(MFRC_DEV dev);
+
+// esp_err_t mfrc_soft_power_down(MFRC_DEV dev);
+
+
+
--- include/MFRC522_Driver.h
+++ include/MFRC522_Driver.h
@@ -23,7 +23,7 @@
 /********* Definitions *****************/
 
 
-#define MRFC_REGADDR_COMMAND    0x01
+#define MRFC_REGADDR_COMMAND            0x01
 #define MRFC_REGADDR_COMM_INTR_EN       0x02
 #define MRFC_REGADDR_DEV_INTR_EN        0x03
 #define MRFC_REGADDR_COMM_IRQ           0x04
@@ -59,13 +59,68 @@
 #define MRFC_REGADDR_MODG_SP            0x29
 #define MRFC_REGADDR_TMR_MODE           0x2A
 #define MRFC_REGADDR_TMR_PRESCALE       0x2B
-#define MRFC_REGADDR_TCNTR_MSB          0x2C
-#define MRFC_REGADDR_TCNTR_LSB          0x2D
+#define MFRC_REGADDR_TCNTR_RELOAD_MSB   0x2C
+#define MFRC_REGADDR_TCNTR_RELOAD_LSB   0x2D
+#define MRFC_REGADDR_TCNTR_MSB          0x2E
+#define MRFC_REGADDR_TCNTR_LSB          0x2F
 
+#define MFRC_REGADDR_TEST_SEL_1         0x31
+#define MFRC_REGADDR_TEST_SEL_2         0x32
+#define MFRC_REGADDR_TEST_PIN_EN        0x33
+#define MFRC_REGADDR_TEST_PIN_VAL       0x34
+#define MFRC_REGADDR_TEST_BUS           0x35
+#define MFRC_REGADDR_TEST_AUTO          0x36
+#define MFRC_REGADDR_VERSION            0x37
+#define MFRC_REGADDR_TEST_ANALOG        0x38
+#define MFRC_REGADDR_TEST_DAC_1         0x39
+#define MFRC_REGADDR_TEST_DAC_2         0x3A
+#define MFRC_REGADDR_TEST_ADC           0x3B
 
+#define MFRC_CMD_IDLE                   0
+#define MFRC_CMD_MEM                    1
+#define MFRC_CMD_GEN_RAND_ID            2
+#define MFRC_CMD_CALC_CRC               3
+#define MFRC_CMD_TRANSMIT               4
+#define MFRC_CMD_NO_CHANGE              7
+#define MFRC_CMD_RECEIVE                8
+#define MFRC_CMD_TRANCEIVE              12
+#define MFRC_CMD_MIFARE_AUTH            15
+#define MFRC_CMD_SOFT_RESET             16
 
+#define MFRC_SPI_ADDR_SHIFT             1
+#define MFRC_SPI_READ_BIT              (1 << 7)
+#define MFRC_FIFO_SIZE_BITS             8 * 64
+#define MFRC_FIFO_SIZE_BYTES            64
+
+#define MFRC_TIMER_INPUT_FREQ_HZ        13560000
 
 /********** Types **********************/
+
+
+typedef enum {
+    MFRC_PWRMODE_ON,
+    MFRC_PWRMODE_STARTING,
+    MFRC_PWRMODE_POWER_DOWN,
+} mfrc_pwrmode_t;
+
+typedef enum {
+    MFRC_RECVR_PWR_ON,
+    MFRC_RECVR_PWR_ANLG_OFF,
+} mfrc_rcvr_pwr_t;
+
+typedef enum {
+    MFRC_TX_PWR_ON,
+    MFRC_TX_PWR_OFF,
+} mfrc_tx_pwr_t;
+
+
+typedef enum {
+    MFRC_CRC_PRESET_0,
+    MFRC_CRC_PRESET_6363,
+    MFRC_CRC_PRESET_A671,
+    MFRC_CRC_PRESET_FFFF,
+    MFRC_CRC_PRESET_INVALID,
+} mfrc_crc_preset_t;
 
 
 typedef enum {
@@ -73,6 +128,16 @@ typedef enum {
     MFRC_I2C_COMMS_MODE,
     MFRC_COMMS_MODE_INVALID,
 } mfrc_comms_mode_t;
+
+typedef enum {
+    MFRC_INTR_SRC_IRQ,
+    MFRC_INTR_SRC_TX_IRQ,
+    MFRC_INTR_SRC_CRC_IRQ,
+    MFRC_INTR_SRC_RX_IRQ,
+    MFRC_INTR_SRC_FIFO_HI_IRQ,
+    MFRC_INTR_SRC_FIFO_LO_IRQ,
+    MFRC_INTR_SRC_ERR_IRQ,
+} mfrc_intr_src_t;
 
 
 typedef struct MFRC_Init
@@ -83,8 +148,535 @@ typedef struct MFRC_Init
     gpio_num_t rst_pin;
     gpio_num_t irq_pin;
 
+    gpio_num_t mosi;
+    gpio_num_t miso;
+    gpio_num_t sel;
+    gpio_num_t clk;
+
+    gpio_num_t sda;
+    gpio_num_t scl;
+
 
 } mfrc_init_t;
+
+
+/** Gonna give this struct register thing a go, might 
+ *  be easier to manager device settings? 
+ **/
+
+typedef struct mfrc_registers {
+
+    /** 0x00 **/
+    uint8_t reserved_0;
+
+    /** 0x01 **/
+    union {
+        uint8_t regval;
+        struct {
+            uint8_t command     :   4;
+            uint8_t pwr_down    :   1;
+            uint8_t rcv_off     :   1;
+            uint8_t rsvd_0      :   2;
+        } bits;
+    } command_reg;
+    
+    /** 0x02 **/
+    union {
+        uint8_t regval;
+        struct {
+            uint8_t tmr_irq_en  :   1;
+            uint8_t err_irq_en  :   1;
+            uint8_t flo_irq_en  :   1;
+            uint8_t fhi_irq_en  :   1;
+            uint8_t idle_irq_en :   1;
+            uint8_t rx_irq_en   :   1;
+            uint8_t tx_irq_en   :   1;
+            uint8_t irq_invert  :   1;
+        } bits;
+    } command_en_reg;
+    
+    /** 0x03 **/
+    union {
+        uint8_t regval;
+        struct {
+            uint8_t rsvd_0      :   2;
+            uint8_t crc_irq_en  :   1;
+            uint8_t rsvd_1      :   1;
+            uint8_t mfin_irq_en :   1;
+            uint8_t rsvd_2      :   2;
+            uint8_t irq_pp_od   :   1;
+        } bits;
+    } irq_passing_reg;
+
+    /** 0x04 **/
+    union {
+        uint8_t regval;
+        struct {
+            uint8_t tmr_irq     :   1;
+            uint8_t err_irq     :   1;
+            uint8_t flo_irq     :   1;
+            uint8_t fli_irq     :   1;
+            uint8_t idle_irq    :   1;
+            uint8_t rx_irq      :   1;
+            uint8_t tx_irq      :   1;
+            uint8_t irq_set     :   1;
+        } bits;
+    } com_irq_reg;
+    
+    /** 0x05 **/
+    union {
+        uint8_t regval;
+        struct {
+            uint8_t rsvd_0      :   2;
+            uint8_t crc_irq     :   1;
+            uint8_t rsvd_1      :   1;
+            uint8_t mfin_irq    :   1;
+            uint8_t rsvd_2      :   2;
+            uint8_t irq_set_2   :   1;
+        } bits;
+    } div_irq_reg;
+
+    /** 0x06 **/
+    union {
+        uint8_t regval;
+        struct {
+            uint8_t protocol_err    :   1;
+            uint8_t parity_err      :   1;
+            uint8_t crc_err         :   1;
+            uint8_t coll_err        :   1;
+            uint8_t buffer_ovr_err  :   1;
+            uint8_t rsvd_0          :   1;
+            uint8_t tempr_err       :   1;
+            uint8_t wrt_err         :   1;
+        } bits;
+    } error_reg;
+
+    /** 0x07 **/
+    union {
+        uint8_t regval;
+        struct {
+            uint8_t fifo_lo_alert   :   1;
+            uint8_t fifo_hi_alert   :   1;
+            uint8_t rsvd_0          :   1;
+            uint8_t tmr_running     :   1;
+            uint8_t irq_alert       :   1;
+            uint8_t crc_ready       :   1;
+            uint8_t crc_ok          :   1;
+            uint8_t rsvd_1          :   1;
+        } bits;
+    } status_1_reg;
+
+    /** 0x08 **/
+    union {
+        uint8_t regval;
+        struct {
+            uint8_t modem_state     :   2;
+            uint8_t mifare_crypt1_on :  2;
+            uint8_t rsvd_0          :   2;
+            uint8_t i2c_force_hs    :   1;
+            uint8_t temp_sens_clr   :   1;
+        } bits;
+    } status_2_reg;
+
+    /** 0x09 **/
+    union {
+        uint8_t regval;
+        struct {
+            uint8_t fifo_data : 8;
+        } bits;
+    } fifo_data_reg;
+
+    /** 0x0A **/
+    union {
+        uint8_t regval;
+        struct {
+            uint8_t fifo_level      :   7;
+            uint8_t flush_buffer    :   1;
+        } bits;
+    } fifo_lvl_reg;
+
+    /** 0x0B **/ 
+    union {
+        uint8_t regval;
+        struct {
+            uint8_t fifo_watermark  :   6;
+            uint8_t rsvd_0          :   2;
+        } bits;
+    } fifo_watermark_reg;
+
+    /** 0x0C **/
+    union {
+        uint8_t regval;
+        struct {
+            uint8_t rx_last_bits    :   3;
+            uint8_t rsvd_0          :   3;
+            uint8_t tmr_start_now   :   1;
+            uint8_t tmr_stop_now    :   1;
+        } bits;
+    } control_reg;
+
+    /** 0x0D **/
+    union {
+        uint8_t regval;
+        struct {
+            uint8_t tx_last_bits    :   3;
+            uint8_t rsvd_0          :   1;
+            uint8_t rx_align        :   3;
+            uint8_t start_send      :   1;
+        } bits;
+    } bitframe_reg;
+
+    /** 0x0E **/
+    union {
+        uint8_t regval;
+        struct {
+            uint8_t collision_pos       :   5;
+            uint8_t coll_pos_invalid    :   1;
+            uint8_t rsvd_0              :   1;
+            uint8_t clr_rx_on_collision :   1;
+        } bits;
+    } collision_reg;
+
+    /** 0x0F **/
+    uint8_t reserved_1;
+
+    /** 0x10 **/
+    uint8_t reserved_2;
+
+    /** 0x11 **/
+    union {
+        uint8_t regval;
+        struct {
+            uint8_t crc_preset         :   2;
+            uint8_t rsvd_0             :   1;
+            uint8_t pol_mf_in          :   1;
+            uint8_t rsvd_1             :   1;
+            uint8_t tx_wait_rf         :   1;
+            uint8_t rsvd_2             :   1;
+            uint8_t crc_msb_first      :   1;
+        } bits;
+    } mode_reg;
+
+    /** 0x12 **/
+    union {
+        uint8_t regval;
+        struct {
+            uint8_t rsvd_0             :   3;
+            uint8_t invert_modulation  :   1;
+            uint8_t tx_speed           :   3;
+            uint8_t tx_crc_en          :   1;
+        } bits;
+    } tx_mode_reg;
+
+    /** 0x13 **/
+    union {
+        uint8_t regval;
+        struct {
+            uint8_t rsvd_0              :   2;
+            uint8_t rx_multiple         :   1;
+            uint8_t rx_no_err           :   1;
+            uint8_t rx_speed            :   3;
+            uint8_t rx_crc_en           :   1;
+        } bits;
+    } rx_mode_reg;
+
+    /** 0x14 **/
+    union {
+        uint8_t regval;
+        struct {
+            uint8_t tx1_rf_en  :  1;
+            uint8_t tx2_rf_en  :  1;
+            uint8_t rsvd_0     :  1;
+            uint8_t tx2_carrier:  1;
+            uint8_t tx1_inv_rf_off : 1;
+            uint8_t tx2_inv_rf_off : 1;
+            uint8_t tx1_inv_rf_on  : 1;
+            uint8_t tx2_inv_rf_on  : 1;
+        } bits;
+    } txcontrol_reg;
+
+    /** 0x15 **/
+    union {
+        uint8_t regval;
+        struct {
+            uint8_t rsvd_0 : 6;
+            uint8_t force_100_ask : 1;
+            uint8_t rsvd_1 : 1;
+        } bits;
+    } tx_ask_reg;
+
+    /** 0x16 **/
+    union {
+        uint8_t regval;
+        struct {
+            uint8_t mf_out_sel  :  4;
+            uint8_t driver_sel  :  2;
+            uint8_t rsvd_0      :  2;
+        } bits;
+    } tx_select_reg;
+
+    /** 0x17 **/
+    union {
+        uint8_t regval;
+        struct {
+            uint8_t rx_wait     :   6;
+            uint8_t uart_sel    :   2;
+        } bits;
+    } rx_select_reg;
+
+    /** 0x18 **/
+    union {
+        uint8_t regval;
+        struct {
+            uint8_t collision_lvl  :  3;
+            uint8_t rsvd_0         :  1;
+            uint8_t min_lvl        :  4;
+        } bits;
+    } rx_threshold_reg;
+
+    /** 0x19 **/
+
+    union {
+        uint8_t regval;
+        struct {
+            uint8_t tau_sync    :  2;
+            uint8_t tau_recv    :  2;
+            uint8_t tmr_prescale_even : 1;
+            uint8_t fix_iq      :  1;
+            uint8_t add_iq      :  2;
+        } bits;
+    } demod_reg;
+
+    /** 0x1A **/
+    uint8_t reserved_3;
+
+    /** 0x1B **/
+    uint8_t reserved_4;
+
+    /** 0x1C **/
+    union {
+        uint8_t regval;
+        struct {
+            uint8_t tx_wait  :  1;
+            uint8_t rsvd_0   :  7;
+        } bits;
+    } mifare_tx_reg;
+
+    /** 0x1D **/
+    union {
+        uint8_t regval;
+        struct {
+            uint8_t rsvd_0 : 4;
+            uint8_t parity_disable : 1;
+            uint8_t rsvd_1 : 3;
+        } bits;
+    } mifare_rx_reg;
+
+    /** 0x1E **/
+    uint8_t reserved_5;
+
+    /** 0x1F **/
+    union {
+        uint8_t regval;
+        struct {
+            uint8_t br_t1 : 5;
+            uint8_t br_t0 : 3;
+        } bits;
+    } serial_speed_reg;
+
+    /** 0x20 **/
+    uint8_t reserved_7;
+
+    /** 0x21 **/ 
+    uint8_t crc_result_msb;
+
+    /** 0x22 **/
+    uint8_t crc_result_lsb;
+
+    /** 0x23 **/
+    uint8_t reserved_8;
+
+    /** 0x24 **/
+    uint8_t mod_width;
+
+    /** 0x25 **/
+    uint8_t reserved_9;
+
+    /** 0x26 **/
+    union {
+        uint8_t regval;
+        struct {
+            uint8_t rsvd_0  :  4;
+            uint8_t rx_gain :  3;
+            uint8_t rsvd_1  :  1;
+        } bits;
+    } rf_cfg_reg;
+
+    /** 0x27 **/
+    union {
+        uint8_t regval;
+        struct {
+            uint8_t mod_gsn  :  4;
+            uint8_t cw_gsn   :  4;
+        } bits;
+    } gsn_reg;
+
+    /** 0x28 **/
+    union {
+        uint8_t regval;
+        struct {
+            uint8_t cw_gsp  :  6;
+            uint8_t rsvd_0  :  2;
+        } bits;
+    } cw_gsp_reg;
+
+    /** 0x29 **/
+    union {
+        uint8_t regval;
+        struct {
+            uint8_t mod_gsp  : 6;
+            uint8_t rsvd  :  2;
+        } bits;
+    } mod_gs_reg;
+
+    /** 0x2A **/
+    union {
+        uint8_t regval;
+        struct {
+            uint8_t tmr_prescale_msb : 4;
+            uint8_t tmr_auto_restart : 1;
+            uint8_t tmr_gated :  2;
+            uint8_t tmr_auto  :  1;
+        } bits;
+    } tmr_mode_reg;
+
+    /** 0x2B **/
+    uint8_t tmr_prescaler_lsb;
+
+    /** 0x2C **/
+    uint8_t tmr_reload_msb;
+
+    /** 0x2D **/
+    uint8_t tmr_reload_lsb;
+
+    /** 0x2E **/
+    uint8_t tmr_cnt_msb;
+
+    /** 0x2F **/
+    uint8_t tmr_cnt_lsb;
+
+    /** 0x30 **/
+    uint8_t reserved_10;
+
+    /** 0x31 **/
+    union {
+        uint8_t regval;
+        struct {
+            uint8_t test_bus_bit_sel : 3;
+            uint8_t rsvd_0           : 5;
+        } bits;
+    } test_sel_1_reg;
+
+    /** 0x32 **/
+    union {
+        uint8_t regval;
+        struct {
+            uint8_t test_bus_sel  : 5;
+            uint8_t prb_s15       : 1;
+            uint8_t prb_s9        : 1;
+            uint8_t test_bus_flip : 1;
+        } bits;
+    } test_sel_2_reg;
+
+    /** 0x33 **/
+    union {
+        uint8_t regval;
+        struct {
+            uint8_t rsvd_0 : 1;
+            uint8_t test_pin_en : 6;
+            uint8_t rs232_line_end : 1;
+        } bits;
+    } testpin_en_reg;
+
+    /** 0x34 **/
+    union {
+        uint8_t regval;
+        struct {
+            uint8_t rsvd_0 : 1;
+            uint8_t test_pin_val : 6;
+            uint8_t use_io : 1;
+        } bits;
+    } testpin_val_reg;
+
+    /** 0x35 **/
+    uint8_t testbus;
+
+    /** 0x36 **/
+    union {
+        uint8_t regval;
+        struct {
+            uint8_t self_test : 4;
+            uint8_t rft  :  2;
+            uint8_t amp_rcv : 1;
+            uint8_t rsvd_0 : 1;
+        } bits;
+    } auto_test_reg;
+
+    /** 0x37 **/
+    union {
+        uint8_t regval;
+        struct {
+            uint8_t version : 4;
+            uint8_t chip_type : 4;
+        } bits;
+    } version_reg;
+
+    /** 0x38 **/
+    union {
+        uint8_t regval;
+        struct {
+            uint8_t analog_sel_aux_2 : 4;
+            uint8_t analog_sel_aux_1 : 4;
+        } bits;
+    } analog_test_reg;
+
+    /** 0x39 **/
+    union {
+        uint8_t regval;
+        struct {
+            uint8_t test_dac_1 : 6;
+            uint8_t rsvd_0 : 2;
+        } bits;
+    } test_dac1_reg;
+
+    /** 0x3A **/
+    union {
+        uint8_t regval;
+        struct {
+            uint8_t test_dac_2 : 6;
+            uint8_t rsvd_0 : 2;
+        } bits;
+    } test_dac2_reg;
+
+    /** 0x3B **/
+    union {
+        uint8_t regval;
+        struct {
+            uint8_t adc_q : 4;
+            uint8_t adc_i : 4;
+        } bits;
+    } adc_test_reg;
+
+    /** 0x3C **/
+    uint8_t reserved_11;
+
+    /** 0x3D **/
+    uint8_t reserved_12;
+
+    /** 0x3F **/
+    uint8_t reserved_13;
+
+} mfrc_register_t;
 
 
 
@@ -96,16 +688,48 @@ typedef struct MFRC522_Driver
     gpio_num_t rst_pin;
     gpio_num_t irq_pin;
 
+    gpio_num_t data_pin;
+    gpio_num_t clock_pin;
+    gpio_num_t select_pin;
+    gpio_num_t data_in_pin;
+
     bool rst_en;
     bool irq_en;
 
+    mfrc_register_t registers;
+
     TaskHandle_t task;
+    spi_device_handle_t spi_handle;
+
+    uint8_t raw_data[64];
+
 
 } MFRC522_Driver_t;
 
 
 typedef MFRC522_Driver_t * MFRC_DEV;
 
-/******** Function Definitions *********/
+/******** Function Declarations *********/
+
+MFRC_DEV mfrc_init(mfrc_init_t *init);
+
+void mfrc_deinit(MFRC_DEV dev);
+
+
+
+// esp_err_t mfrc_set_interrupt_mask(MFRC_DEV dev, uint8_t *intr);
+
+// esp_err_t mfrc_get_interrupt_mask(MFRC_DEV dev, uint8_t *intr);
+
+// esp_err_t mfrc_enable_interrupt_pin(MFRC_DEV dev);
+
+// esp_err_t mfrc_disable_interrupt_pin(MFRC_DEV dev);
+
+// esp_err_t mfrc_soft_power_down(MFRC_DEV dev);
+
+
+
+
+
 
 #endif /* HEADER_H */
